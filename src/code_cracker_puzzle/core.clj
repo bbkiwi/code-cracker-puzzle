ce84cc06a080944c702c694dcddcf745e1fb1d32
bbkiwi <staf186@ext.canterbury.ac.nz>
2016-05-11 23:03:32 +1200

    Routines sorting clues Data in resources

--- src/code_cracker_puzzle/core.clj
+++ src/code_cracker_puzzle/core.clj
@@ -3,20 +3,25 @@
     (:require [clojure.string :as str]
               [clojure.set :as set]
               [clojure.edn :as edn]                         ;safe io
-              [clojure.core.matrix :as m]))
-
+              [clojure.core.matrix :as m]
+              [clojure.walk]))
+; TODO left out [clojure.walk] above and wouldn't work with lein run but now error without it???
+; even with it still does not run but gets different error messaage
 
 (defn myvars [] (keys (ns-interns *ns*))) ; use (myvars) to see what's in *ns*
 
 (def word-dic
-  (slurp "D:\\Bill\\My Documents\\UCmatlab\\CodeCracker\\WordLists\\2of4brifblanksep.txt"))
+  "string starting with blank consisting of words each followed by blank"
+  ;(slurp "D:\\Bill\\My Documents\\UCmatlab\\CodeCracker\\WordLists\\2of4brifblanksep.txt")
+  (slurp "resources/2of4brifblanksep.txt"))
 
 (def CCdata
-  (slurp "D:\\Bill\\My Documents\\UCmatlab\\CodeCracker\\CCdata.txt"))
+  ;(slurp "D:\\Bill\\My Documents\\UCmatlab\\CodeCracker\\CCdata.txt")
+  (slurp "resources/CCdata.txt"))
 
 (defn findall
   "Uses regex-pat which has group 1 equal the words sought to search dictionary dic, a string of words
-   separated by single blanks. Returns lazy sequence (possibly empty) of words from group 1.
+   separated by single blanks (also starting and ending with a blank. Returns lazy sequence (possibly empty) of words from group 1.
    #\" (regexforword)(?= \"   "
   [regex-pat dic]
   ;(println regex-pat)
@@ -91,7 +96,7 @@
 (defn code-cracker-pat-to-regexpat
   "Converts code cracker pattern string to regex. Assume letters assigned sequentially from 1 to 8
   0 is a free letter. Pattern starts with blank which is start of word in the dictionary.
-  The whole word is group 1, then code crackers unknowns are assinge groups 2 up to 8.
+  The whole word is group 1, then code crackers unknowns are assigned groups 2,...,9.
   Example \"b12t\"  goes to
   #\" (b([abdefghijklmnopqrsuvwxyz])(?!\\2)([abdefghijklmnopqrsuvwxyz])t)(?= )\"
   Note no checks on validity of code cracker pattern done - code letters must start
@@ -100,7 +105,7 @@
   [code-cracker-pat letuse]
   (let
     [front-pat ""
-     char-pat (first code-cracker-pat)                      ; not work if code-cracker-pat is vec
+     char-pat (first code-cracker-pat)  ; not work if code-cracker-pat is vec
      rest-pat (str/join "" (rest code-cracker-pat))]
     (re-pattern (str
                   " ("
@@ -161,6 +166,8 @@
   [assigned-letters-map clue word-found]
   (merge assigned-letters-map (zipmap clue word-found)))
 
+;TODO have clue be map with :vec of code number and :dic of words that match (use findall)
+;TODO so speed up ranking and filtering
 
 ; version making lazy sequences representing tree with levels from each clue
 (defn code-solver
@@ -176,6 +183,8 @@
           code-cracker-pat (make-code-cracker-pat decoded-vec)
           regexpat (code-cracker-pat-to-regexpat code-cracker-pat letuse)
           words-found (findall regexpat word-dic)]
+      ;TODO findall replaced by filter words associated with a (:dec clue)
+      ;TODO maybe dic can be seq of words rather then one string
       ;(println "Letters to Use " letuse)
       ;(println "Decoded Vector " decoded-vec)
       ;(println "Code Cracker Pattern " code-cracker-pat)
@@ -183,23 +192,18 @@
       ;(println "Found " words-found)
       ; TODO only conj if non-empty updated map to add
       (reduce #(lazy-seq (conj %1 (update-assigned-letters-map assigned-letters-map clue %2))) (lazy-seq) words-found))
+ ;TODO want in recursion to use refined :dic
     (clojure.walk/postwalk #(if (map? %) (code-solver clue %) %) assigned-letters-map)))
 
 
-; TODO this version takes clues from clue-set in order given
-; however there is a ranking of clues for an assigned-letters-map
-; it should be the same for each map in the coll of maps
-; the best should be taken first, so need to reduce in that order
-; and recalculate the ranking each time
-
 (defn code-solver-from-clues
+  "Take clues from clue-set in order given."
   [clue-set assigned-letters-map-coll]
   (reduce #(code-solver %2 %1) assigned-letters-map-coll clue-set))
 
-;  score by number of different letters needed to complete less 1  over total length
-;  zero if one letter left, neg if complete, always < 1
-;  check all completed words and  assign positive test score of 1 if not in dictionary 2 if in dictionary
 (defn score-clue
+  "score by number of different letters needed to complete less 1 over total length
+   zero if one letter left, neg if complete, always < 1"
   [clue  decode-map]
   (->>
     (decode-to-vec clue decode-map)
@@ -209,29 +213,86 @@
     (dec)
     (* (/ (count clue)))))
 
+
+
+(defn complete?
+  "true iff all letters of clue filled in"
+  [clue decode-map]
+  (->>
+    (decode-to-vec clue decode-map)
+    (filter number?)
+    (empty?)))
+
+
 (defn rank-n-sort-clues
+  "removes completed clues and sorts remaining by most likely to have few matches"
   [clues decode-map]
   (->>
     clues
-    (filter #(>= (score-clue % decode-map) 0))
+    ;(filter #(>= (score-clue % decode-map) 0))
+    (filter #((complement complete?) % decode-map))
     (sort-by #(score-clue % decode-map))))
 
 
+(defn find-all-words
+  "Takes clue (a vector of code numbers) and a partial solution map assigned-letters-map
+   Returns a lazy seq  of each solution word."
+  [clue assigned-letters-map]
+  (let [letuse (clean-letuse "" (assigned-letters-map->assigned-letters-string assigned-letters-map))
+        decoded-vec (decode-to-vec clue assigned-letters-map)
+        code-cracker-pat (make-code-cracker-pat decoded-vec)
+        regexpat (code-cracker-pat-to-regexpat code-cracker-pat letuse)]
+    (findall regexpat word-dic)))
 
-; TODO check further, using simple rank, not using clue that gives unique
-; not sure if get to words not in dictionary
+(defn score-clue-on-count
+  "completed clues give 3 if in dictionary, 2 if not in dictionary,
+  give 1 if no words match uncompleted clue
+  give 1 - 1/(number of words found upto 30)"
+  [clue decode-map]
+  (let [cnt (count (take 30 (find-all-words clue decode-map)))]
+    (if (complete? clue decode-map)
+      (+ 2 cnt)
+      (if (zero? cnt)
+        1
+        (inc (- (/ cnt)))))))
+
+(defn rank-filtered-n-sort-clues-on-count
+  "removes completed clues and sorts remaining by fewest matches first"
+  [clues decode-map]
+  (->>
+    clues
+    ;(filter #(<= (score-clue-on-count % decode-map) 1))
+    (filter #((complement complete?) % decode-map))
+    (sort-by #(score-clue-on-count % decode-map))))
+
+(defn rank-n-sort-clues-on-count
+  "sorts all clues by fewest matches first for non completed  clues
+   followed by completed clues that are not in dictionary then those in dictionary"
+  [clues decode-map]
+  (sort-by #(score-clue-on-count % decode-map) clues))
+
+(defn extract-leaves
+  "tree is coll representing a tree with assigned-letter-maps as leaves
+  outputs a coll of the leaves"
+  [tree]
+  (filter map? (tree-seq (complement map?) identity tree)))
+
+
 (defn code-solver-using-best-clue
-  [clues assigned-letters-map-coll]
-  (let [leaves (filter map? (tree-seq (complement map?) identity  assigned-letters-map-coll))
+  "Recursively solves"
+  [clues assigned-letters-map-coll sort-function]
+  (let [leaves (extract-leaves assigned-letters-map-coll)
         letmaptosort (first leaves)
-        sortclues (rank-n-sort-clues clues letmaptosort)]
+        sortclues (sort-function clues letmaptosort)]
     ;(println letmaptosort)
     ;(println sortclues)
+    (println assigned-letters-map-coll)
     (if (first sortclues)
       (code-solver-using-best-clue clues
                                    (code-solver
                                      (first sortclues)
-                                     assigned-letters-map-coll))
+                                     assigned-letters-map-coll)
+                                   sort-function)
       leaves)))
 
 ; Examples
@@ -244,6 +305,51 @@
 ; TODO make code-solver-using-best-clue
 ; rank clues, can from best to worse finding one with smallest number of matches,
 ; use it and recurse - terminate when all rankings are positive.
+
+; version making lazy sequences representing tree with levels from each clue
+
+(defn code-solver-from-all-clues
+  "Takes the best (wrt to sort-function) clue from clues (a lazy seq of vectors of code numbers) and a
+   partial solution map assigned-letters-map.
+   Returns a lazy seq representing a tree with leaves the maps that correspond to
+   each solution word."
+  [clues assigned-letters-map sort-function]
+  ;(println clues assigned-letters-map)
+  (if
+    (map? assigned-letters-map)   ; map or else lazy seq tree of maps
+    (let [letuse (clean-letuse "" (assigned-letters-map->assigned-letters-string assigned-letters-map))
+          ;sortclues (rank-n-sort-clues-on-count clues assigned-letters-map)
+          ;sortclues (rank-n-sort-clues clues assigned-letters-map)
+          sortclues (sort-function clues assigned-letters-map)
+          bestclue (first sortclues)
+          decoded-vec (decode-to-vec bestclue assigned-letters-map)
+          code-cracker-pat (make-code-cracker-pat decoded-vec)
+          regexpat (code-cracker-pat-to-regexpat code-cracker-pat letuse)
+          words-found (findall regexpat word-dic)]
+      ;(println bestclue)
+      ;(println words-found)
+      (reduce #(lazy-seq (conj %1 (update-assigned-letters-map assigned-letters-map bestclue %2))) (lazy-seq) words-found))
+    ;TODO want in recursion to use refined :dic
+    (clojure.walk/postwalk #(if (map? %) (code-solver-from-all-clues clues % sort-function) %) assigned-letters-map)))
+
+
+; to manually look at interations in repl
+;(def sols (iterate #(code-solver-from-all-clues (:clues cc) % rank-n-sort-clues-on-count) (:encodemap cc)))
+;(show-sol 0 (filter map? (tree-seq (complement map?) identity  (nth sols 15))) (:clues cc))
+
+
+(defn iterate-code-solver-from-all-clues
+  "Recursively uses code-solver-from-all-clues"
+  [clues assigned-letters-map sort-function]
+  (loop
+    [output assigned-letters-map
+     alm (code-solver-from-all-clues clues assigned-letters-map sort-function)]
+    (println alm)
+    (if (empty? (extract-leaves alm))
+      (extract-leaves output)
+      (recur alm (code-solver-from-all-clues clues alm sort-function)))))
+
+
 
 (defn brute-solver-from-4-clues
   [clues assigned-letters-map-tree]
@@ -264,12 +370,13 @@
   [sentence]
   (let [clues (map encode (str/split (str/lower-case sentence) #" +"))
         solutions (code-solver-from-clues clues #{{}})
-        solutions (tree-seq (complement map?) identity solutions)
-        solutions (filter map? solutions)]
+        solutions (extract-leaves solutions)]
     (println clues)
-    ;(println "Found " (count solutions) " solutions")
+    (println "Found " (count solutions) " solutions")
+    (println solutions)
     (map (fn [s] (str/join " " (map #(decode %1 s) clues))) solutions)))
 
+;(make-example "pas pals clap sap lap slap claps pal")
 
 ;(def tt (fn [v] (filter #(and (> (count %) 1) (not (contains? (set %) 0))) (partition-by zero? v))))
 
@@ -315,7 +422,7 @@
 
 
 
-;(make-example "pas pals clap sap lap slap claps pal")
+
 
 ;TODO finding have to constantly change type so can get commands to work!
 ;TODO finding editing constantly changing how parens go and muck up code
@@ -339,9 +446,8 @@
 ; do some filtering out of empty maps
 ; use tree-seq to explore
 (defn -main
-  [numcc]
-  (let [cc (get-cc numcc)]
-    (code-solver-from-clues (:clues cc) (:encodemap cc))))    ; will be very slow
+  [n]
+  (println "Main" n))
 
 
 ;(def ans (brute-solver-from-4-clues [[1 2 3 2 1],[4 5 6 4],[1 6 5], [1 5 6]] {} ))
@@ -383,16 +489,39 @@
 
 ; Examples - some work n 1 ... 48 other stack overflow
 (defn solve-example-cc
-  [n]
+  "n is problem number in CCdata
+   cc-solve-method is iterate-code-solver-from-all-clues or code-solver-using-best-clue
+   sort-function is rank-n-sort-clues, rank-n-sort-clues-on-count or rank-filtered-n-sort-clues-on-count"
+  [n cc-solve-method sort-function]
   (let [cc (get-cc n)
-        ans (code-solver-using-best-clue (:clues cc) (:encodemap cc))]
+        ans (time (cc-solve-method
+                    (:clues cc)
+                    (:encodemap cc)
+                    sort-function))]
     (println (count ans))
-    (time (show-sol 0 ans (:clues cc)))))
+    (show-sol 0 ans (:clues cc))))
+
+; problem 1
+;(solve-example-cc 1 iterate-code-solver-from-all-clues rank-n-sort-clues) ; solves "Elapsed time: 402 msecs"
+;(solve-example-cc 1 iterate-code-solver-from-all-clues rank-n-sort-clues-on-count) ; inf loop - final map just moves deeper down tree
+;(solve-example-cc 1 iterate-code-solver-from-all-clues rank-filtered-n-sort-clues-on-count) ; solves 9256 msecs
+;(solve-example-cc 1 code-solver-using-best-clue rank-n-sort-clues) ; solves "Elapsed time: 368.754859 msecs"
+;(solve-example-cc 1 code-solver-using-best-clue rank-n-sort-clues-on-count) ;inf loop -
+;(solve-example-cc 1 code-solver-using-best-clue rank-filtered-n-sort-clues-on-count) ;solved 9206 msecs
 
 
+; problem 3 has a bad word
+;(solve-example-cc 3 iterate-code-solver-from-all-clues rank-n-sort-clues) ; premature stops "Elapsed time: 146.010114 msecs"
+;(solve-example-cc 3 iterate-code-solver-from-all-clues rank-n-sort-clues-on-count) ; solves "Elapsed time: 15817.357202 msecs"
+;(solve-example-cc 3 iterate-code-solver-from-all-clues rank-filtered-n-sort-clues-on-count) ; solves "Elapsed time: 18676 msecs"
+;(solve-example-cc 3 code-solver-using-best-clue rank-n-sort-clues) ; after 10 recursions gets tree of empty maps and stackoverflow
+;(solve-example-cc 3 code-solver-using-best-clue rank-n-sort-clues-on-count) ;same as above but slower
+;(solve-example-cc 3 code-solver-using-best-clue rank-filtered-n-sort-clues-on-count) ;solved 8661 msecs
 
 
 ; observation
+; (def x [...])
+; (distinct? x) ;true
 ; (apply distinct? (distinct x))  ;true
 
 ; comment with line comment should be toggle ...
